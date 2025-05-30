9c654e934c670afe9fdbb62ec818da16dce642fe
wintec-yuda <alamasta004@gmail.com>
2025-05-31 06:52:24 +0700

    Add Flask web app

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('Hello, DevOps from Python!')
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Selamat Datang di Kafe DevOps!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
